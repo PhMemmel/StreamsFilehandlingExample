c62592f58994fb79c226a9d259e9bf36c5160646
Philipp Memmel <philipp.memmel@thg.muenchen.musin.de>
2021-10-16 10:28:59 +0200

    fix handling csv files using quotes around columns

--- src/nqgy/sep/streams/utils/ExtractUtils.java
+++ src/nqgy/sep/streams/utils/ExtractUtils.java
@@ -7,9 +7,8 @@ public class ExtractUtils {
     String column = line.split(";")[columnNumber].trim();
     // the following is just to remove eventually existing leading or trailing quotes which
     // csv files happen to use to encapsulate the columns
-    if (column.substring(0, 0).equals("\"")
-        && column.substring(column.length() - 2, column.length() - 1).equals("\"")) {
-      column = column.substring(1, column.length() - 2);
+    if (column.startsWith("\"") && column.endsWith("\"")) {
+      column = column.substring(1, column.length() - 1);
     }
     return column;
   }
